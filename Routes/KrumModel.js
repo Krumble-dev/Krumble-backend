a5793d60f03a6bb6a40a6ed7d3886007445f4530
Dhanarooban1 <dhanazx1@gmail.com>
2025-02-01 22:50:14 +0530

    Added

--- Routes/KrumModel.js
+++ Routes/KrumModel.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import authenticaToken from '../Middleware/auth.js';
 import multer from 'multer';
-// import KrumModelControllers from '../Controllers/krumModel.controllers.js';
+
 import krumModelControllers from '../Controllers/krumModel.controllers.js';
 
 
@@ -12,9 +12,9 @@ router.use(authenticaToken);
 // router.post('/drop', upload.single('file'), dropARModel); 
 // router.post('/collect/:modelId', collectARModel); 
 
-router.post('/create',upload.single('file'), KrumModelControllers.uploadKrumModel);
-router.get('/getkrummodels', KrumModelControllers.getKrumModels);
-router.delete('/deletekrummodel', KrumModelControllers.deleteKrumModel);
+router.post('/create',upload.single('file'), krumModelControllers.uploadKrumModel);
+router.get('/getkrummodels', krumModelControllers.getKrumModels);
+router.delete('/deletekrummodel', krumModelControllers.deleteKrumModel);
 
 
 export default router;
